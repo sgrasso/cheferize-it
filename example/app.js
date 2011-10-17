73a584109204d855e1bcf0127bd3405df2d70ddc
Sean <sean.grasso@gmail.com>
2011-10-16 23:36:27 -0400

    Addition of Translation rules and app.js code

---
+++ example/app.js
@@ -0,0 +1,19 @@
+/**
+ * Module dependencies.
+ */ 
+var http = require("http"),
+	url = require("url"),
+	chef = require("../lib/cheferizeIt");
+
+// Create node.js server
+http.createServer(function(req, res) {
+	// This grabs the pathname after the base url from the request.
+	var strOut = "";
+	var uri = url.parse(req.url).pathname;
+	uri = uri.replace("/","");
+	if (uri !== "favicon.ico"){
+		strOut = chef.cheferize(uri);
+	}
+	res.write(strOut);
+	res.end();
+}).listen(4000);
