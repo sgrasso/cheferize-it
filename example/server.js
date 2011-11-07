25b345b2bf68beb3be3ac60019a7f9a0f7ac7b07
Sean <sean.grasso@gmail.com>
2011-11-06 22:53:22 -0500

    Fix nodester port

---
+++ example/server.js
@@ -0,0 +1,21 @@
+/**
+ * Module dependencies.
+ */ 
+var http = require("http"),
+	url = require("url"),
+	chef = require("../lib/cheferizeIt");
+
+// Create node.js server
+http.createServer(function(req, res) {
+	// This grabs the pathname after the base url from the request.
+	var strOut = "";
+	var uri = url.parse(req.url).pathname;
+	uri = uri.replace("/","");
+	if (uri !== "favicon.ico"){
+		strOut = chef.cheferize(uri);
+		strOut = strOut.replace(/%20/g," ");
+	}
+	console.log(strOut);
+	res.write(strOut);
+	res.end();
+}).listen(12449);
